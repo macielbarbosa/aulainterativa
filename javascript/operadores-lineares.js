1f35a2da0589342adcf92932db0d8f7b61cf2e54
RESIDENTE <barbosa@outlook.com>
2017-05-04 15:13:29 -0300

    operadores mod1

--- javascript/operadores-lineares.js
+++ javascript/operadores-lineares.js
@@ -4,6 +4,12 @@ var applet = new GGBApplet(parameters, '5.0');
 window.onload = function() {
 	applet.inject('applet_container');
 }
+
+/*$('#op1').change(function() {
+    var val = $("#op1 option:selected").text();
+    alert(val);
+});*/
+
 function figure(){
 	ggb = document.cVW7D8fU;
 	dados = document.getElementById('dados');
@@ -52,227 +58,38 @@ function figure(){
 		ggb.setVisible('E',true);
 		ggb.setVisible('F',true);
 	}
-	/*
-	ggb.evalCommand('Pz = ('+ coordenadas +')');
-	vx = ggb.getXcoord('Pz');
-	vy = ggb.getYcoord('Pz');
-	vz = ggb.getZcoord('Pz');
-	moduloV = Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2)+Math.pow(vz,2));
-	ggb.setCoordSystem(-moduloV,moduloV,-moduloV,moduloV,-moduloV,moduloV,false);
-	document.getElementById('v').innerHTML = '<br/><b>v</b> = ('+vx+', '+vy+', '+vz+')<br/><br/>| <b>v</b> | = '+moduloV.toFixed(2)+'<br/><br/>';
 	
-	document.getElementById("matrizV").innerHTML = '`[['+vx+'],['+vy+'],['+vz+']]`';
-	document.getElementById('v').style.display = 'block';
-	document.getElementById("v'").style.display = 'none';
-	document.getElementById("matrizRotZ").style.display = 'none';
-	document.getElementById("matrizRotY").style.display = 'none';
-	document.getElementById("matrizRotX").style.display = 'none';
-	document.getElementById("demo").style.display = 'none';
-
-	ggb.evalCommand('v = Pz');
-	ggb.setVisible('v',true);
-	ggb.setVisible('cX',false);
-	ggb.setVisible('cY',false);
-	ggb.setVisible('cZ',false);
-	ggb.setValue('tx',0);
-	ggb.setValue('ty',0);
-	ggb.setValue('tz',0);
-	ggb.setVisible("v'",false);*/
 }
 function executar(){
-	operador = dados.elements[2].value;
+	operador1 = dados.elements[2].value;
+	operador2 = dados.elements[4].value;
+	operador3 = dados.elements[6].value;
 	
-	if(operador=='reflexao'){
-		
-	}
-	else if(operador=='dilatacao'){
-		
-	}
-	else if(operador=='rotacao'){
-		
-	}
-	else if(operador=='cisalhamento'){
-		
-	}
-	
-	atualizarComponentes();
-	matrizesRotacao();
-	document.getElementById("v'").style.display = 'block';
-	document.getElementById("demo").style.display = 'block';
-	MathJax.Hub.Typeset(); //atualizar notacao asciimath
+	operacao(operador1,'escalar1');
+	operacao(operador2,'escalar2');
+	operacao(operador3,'escalar3');
+
 }
-/*function rotacionarZ(){
-	rotacaoNegativa('z');
-	ggb.evalCommand("v' = Qz");
-	ggb.evalCommand('nz = '+Math.abs(nz));
-	if((!rotY || !ny) && (!rotX || !nx))
-		ggb.setVisible('cZ',true);
-	ggb.setVisible("v'",true);
-	ggb.evalCommand('StartAnimation[tz]');
-}
-function rotacionarY(){
-	rotacaoNegativa('y');
-	if(rotZ && nz)
-		ggb.evalCommand('Py = Rotate[Pz,'+nz+'°,zAxis]');
-	else
-		ggb.evalCommand('Py = ('+coordenadas+')');
-	ggb.evalCommand("v' = Qy");
-	ggb.evalCommand('ny = '+Math.abs(ny));
-	if((!rotZ || !nz) && (!rotX || !nx))
-		ggb.setVisible('cY',true);
-	ggb.setVisible("v'",true);
-	ggb.evalCommand('StartAnimation[ty]');
-}
-function rotacionarX(){
-	rotacaoNegativa('x');
-	if(ny && rotY)
-		ggb.evalCommand('Px = Rotate[Py,'+ny+'°,yAxis]');
-	else if(rotZ && nz)
-		ggb.evalCommand('Px = Rotate[Pz,'+nz+'°,zAxis]');
-	else
-		ggb.evalCommand('Px = ('+coordenadas+')');
-	ggb.evalCommand("v' = Qx");
-	ggb.evalCommand('nx = '+Math.abs(nx));
-	if((!rotZ || !nz) && (!rotY || !ny))
-		ggb.setVisible('cX',true);
-	ggb.setVisible("v'",true);
-	ggb.evalCommand('StartAnimation[tx]');
-}
-function rotacaoNegativa(eixo){
-	if(eixo=='z'){
-		if(nz<0){	
-			ggb.evalCommand('Qz=Rotate[Pz,(-tz)°,zAxis]');
-			ggb.evalCommand('α=Angle[Qz,Cz,Pz,zAxis]');
-			ggb.evalCommand('Mz=Rotate[Pz,-0.001°,zAxis]');
+
+function operacao(operador,escalar){
+	if(operador!='nenhum'){	
+		if(operador=='reflexaoX'){
+			
 		}
-		else{
-			ggb.evalCommand('Qz=Rotate[Pz,tz°,zAxis]');
-			ggb.evalCommand('α=Angle[Pz,Cz,Qz,zAxis]');
-			ggb.evalCommand('Mz=Rotate[Pz,0.001°,zAxis]');
+		else if(operador=='reflexaoY'){
+			
 		}
-	}
-	else if(eixo=='y')
-		if(ny<0){	
-			ggb.evalCommand('Qy=Rotate[Py,(-ty)°,yAxis]');
-			ggb.evalCommand('β=Angle[Qy,Cy,Py,yAxis]');
-			ggb.evalCommand('My=Rotate[Py,-0.001°,yAxis]');
+		else if(operador=='dilatacao'){
+			
 		}
-		else{
-			ggb.evalCommand('Qy=Rotate[Py,ty°,yAxis]');
-			ggb.evalCommand('β=Angle[Py,Cy,Qy,yAxis]');
-			ggb.evalCommand('My=Rotate[Py,0.001°,yAxis]');
+		else if(operador=='rotacaoX'){
+			
 		}
-	else{
-		if(nx<0){	
-			ggb.evalCommand('Qx=Rotate[Px,(-tx)°,xAxis]');
-			ggb.evalCommand('γ=Angle[Qx,Cx,Px,xAxis]');
-			ggb.evalCommand('Mx=Rotate[Px,-0.001°,xAxis]');
+		else if(operador=='rotacaoY'){
+			
 		}
-		else{
-			ggb.evalCommand('Qx=Rotate[Px,tx°,xAxis]');
-			ggb.evalCommand('γ=Angle[Px,Cx,Qx,xAxis]');
-			ggb.evalCommand('Mx=Rotate[Px,0.001°,xAxis]');
+		else if(operador=='cisalhamento'){
+			
 		}
 	}
 }
-function atualizarComponentes(){
-	rotacaoV();
-	var x = vx;
-	var y = vy;
-	var z = vz;			
-	var modulo = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-	if(Math.abs(Math.round(x)-x)<0.005)
-		x = Math.round(x);
-	else
-		x = x.toFixed(2);
-	if(Math.abs(Math.round(y)-y)<0.005)
-		y = Math.round(y);
-	else
-		y = y.toFixed(2);
-	if(Math.abs(Math.round(z)-z)<0.005)
-		z = Math.round(z);
-	else
-		z = z.toFixed(2);
-	document.getElementById("v'").innerHTML = "<br/><b>v</b>' = ("+x+", "+y+", "+z+")<br/><br/>| <b>v</b>' | = "+modulo.toFixed(2)+"<br/><br/>";
-	document.getElementById("matrizV'").innerHTML = '`[['+x+'],['+y+'],['+z+']]`';
-}
-function rotacaoV(){
-	if(nz && rotZ){
-		rotacaoVz();
-		if(ny && rotY)
-			rotacaoVy();
-		if(nx && rotX)
-			rotacaoVx();
-	}
-	else if(ny && rotY){
-		rotacaoVy();
-		if(nx && rotX)
-			rotacaoVx();
-	}
-	else if(nx && rotX)
-		rotacaoVx();
-}
-function rotacaoVz(){
-	var x = vx, y = vy, z = vz; //não usar o valor alterado
-	vx = x*Math.cos(nz*Math.PI/180) - y*Math.sin(nz*Math.PI/180);
-	vy = x*Math.sin(nz*Math.PI/180) + y*Math.cos(nz*Math.PI/180);
-}
-function rotacaoVy(){
-	var x = vx, y = vy, z = vz; //não usar o valor alterado
-	vx = x*Math.cos(ny*Math.PI/180) + z*Math.sin(ny*Math.PI/180);
-	vz = -x*Math.sin(ny*Math.PI/180) + z*Math.cos(ny*Math.PI/180);
-}
-function rotacaoVx(){
-	var x = vx, y = vy, z = vz; //não usar o valor alterado
-	vy = y*Math.cos(nx*Math.PI/180) - z*Math.sin(nx*Math.PI/180);
-	vz = y*Math.sin(nx*Math.PI/180) + z*Math.cos(nx*Math.PI/180);
-}
-function matrizesRotacao(){
-	var matrizRotZ = document.getElementById('matrizRotZ');
-	var matrizRotY = document.getElementById('matrizRotY');
-	var matrizRotX = document.getElementById('matrizRotX');
-	if(rotZ && nz){
-		matrizRotZ.innerHTML = '`[[cos'+nz+'\"&deg;\",-sin'+nz+'\"&deg;\",0],[sin'+nz+'\"&deg;\",cos'+nz+'\"&deg;\", 0],[0,0,1]]xx`';					
-		matrizRotZ.style.display = 'inline';
-	}
-	if(rotY && ny){
-		matrizRotY.innerHTML = '`[[cos'+ny+'\"&deg;\",0,sin'+ny+'\"&deg;\"],[0,1,0],[-sin'+ny+'\"&deg;\",0,cos'+ny+'\"&deg;\"]]xx`';
-		matrizRotY.style.display = 'inline';
-	}
-	if(rotX && nx){
-		matrizRotX.innerHTML = '`[[1,0,0],[0,cos'+nx+'\"&deg;\",-sin'+nx+'\"&deg;\"],[0,sin'+nx+'\"&deg;\",cos'+nx+'\"&deg;\"]]xx`';
-		matrizRotX.style.display = 'inline';
-	}
-	var eqRot = document.getElementById('eqRot');
-	if(nz && rotZ){
-		eqRot.innerHTML = '`v\' = R_alpha"&nbsp;"v`';
-		if(ny && rotY)
-			eqRot.innerHTML = '`v\' = R_betaR_alpha"&nbsp;"v`';
-		else if(nx && rotX)
-			eqRot.innerHTML = '`v\' = R_gammaR_alpha"&nbsp;"v`';
-		if(ny && rotY && nx && rotX)
-			eqRot.innerHTML = '`v\' = R_gammaR_betaR_alpha"&nbsp;"v`';
-	}
-	else if(ny && rotY) {
-		eqRot.innerHTML = '`v\' = R_beta"&nbsp;"v`';
-		if(nx && rotX)
-			eqRot.innerHTML = '`v\' = R_gammaR_beta"&nbsp;"v`';
-	}
-	else if(nx && rotX)
-		eqRot.innerHTML = '`v\' = R_gamma"&nbsp;"v`';
-}
-function coordenadasV(){
-	var virgulas = 0;
-	var letras = 0;
-	for(var i=0; i<coordenadas.length; i++){
-		if(i!=coordenadas.length-1 && coordenadas.charAt(i)=='.' && coordenadas.charAt(i+1)=='.')
-			return false;
-		else if(coordenadas.charAt(i)==' ' || coordenadas.charAt(i)=='.')
-			continue;
-		else if(coordenadas.charAt(i)==',')
-			virgulas++;
-		else if(coordenadas.charAt(i)<'0' || coordenadas.charAt(i)>'9')
-			letras++;
-	}
-	return virgulas==2 && letras==0;
-}*/
