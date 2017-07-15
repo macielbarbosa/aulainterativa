7e84c6df86f4f5fe0967df2729751484528a9cdb
macielbarbosa <macielbarbosa@outlook.com>
2017-07-15 14:02:57 -0600

    modificacoes finais

---
+++ javascript/intersecao-quadricas.js
@@ -0,0 +1,158 @@
+var param = {
+	"id":"ggb",
+	"width":1200,
+	"height":500,
+	"showToolBar":false,
+	"borderColor":null,
+	"showMenuBar":false,
+	"showAlgebraInput":false,
+	"showResetIcon":false,
+	"enableLabelDrags":false,
+	"enableShiftDragZoom":true,
+	"enableRightClick":false,
+	"capturingThreshold":null,
+	"showToolBarHelp":false,
+	"errorDialogsActive":false,
+	"useBrowserForJS":true,
+	"ggbBase64":"UEsDBBQACAgIAAUG70oAAAAAAAAAAAAAAAAWAAAAZ2VvZ2VicmFfamF2YXNjcmlwdC5qc0srzUsuyczPU0hPT/LP88zLLNHQVKiu5QIAUEsHCEXM3l0aAAAAGAAAAFBLAwQUAAgICAAFBu9KAAAAAAAAAAAAAAAAFwAAAGdlb2dlYnJhX2RlZmF1bHRzMmQueG1s7Zrdc9soEMCf27+C0dPdQ2xJtmwnE6eTdubmMpOmN+dM516xtJa5YNAJVMv564tAX3asnCM7n00eIhYBgt8uywI+/ZQuKPoBsSCcjS2nY1sImM8DwsKxlcjZ0cj6dPbxNAQewjTGaMbjBZZjy8tKlvWU1OkNh1keSgU5YfwKL0BE2IeJP4cFvuQ+lrroXMropNtdLpedotEOj8NuGMpOKgILqQ4xMbbyxIlqbq3SsqeLu7btdP/5emmaPyJMSMx8sJDqbAAznFApVBIoLIBJJFcRjK2I01XImYUongIdW38Vcl5jbA1t6+zjh1NKGEzkigKSc+LfMBCqRz0rb8Y2iT9JEEAGzepmdcScLxGf/gu+akfGCZSf0YIuo15/4ZTHKFbVPNWgguw5FprqRjGN5lilOo5t/pz+se04A8c1tSleQYx+YJqV1jk4kdzXDercGaYCirLq0195AOZNPy/PyELrAQkJUdZ1JCKAQKfKwUXqQ1r/tfaImMxxBBvfOe3mgLeiziiusTYZFWznyWDbGrV9h7T93GSbCc4S5mcNXn3HcTk6llBaIzjwrDY0XLvfwGPoPTeQNqZGmKzZmZLQb7MY4PcaKcduRapuN67nPZPlOA8B9eHU5zwOBErH1hW+stAqf96apy6imU3IbV7Zq+fqqViN6IHgA4iAqakq1+g7regPRhp/9piax9vE32uFnxJR0b/UQgW8t8XeTR93MXi1AG24hiPn+X1lM/I2S8huU+DIefAc+FYPKNTa3So4cD2jiOz57nt24n7B/oaQrIVzTu+d/hPRX7f6/vtqW2Ouixi6Ivs/tny+iCikB1SJgDCTSsKTQi6V4rZbhF9s6LyXStqsGDyRNOvDBZNq4ws6Ohd3Pn4DEF2ryt/YdYyZyDbMpkyB974VvbZhulzfLLn7B7C/tvbgP7Y2BYmagcQn8n9Uwv1EVDoxUqmU0S+jFJykhBIcrw4Qe+2353XbrenNa4v7Cve8MV7d5+nbHQu8QrN8rZ6+MPRGI98/gHolGjyk+/ihOPHKcXzPxZLqtk3x26T6SPNiSyCLYwmCYHa/ZiSkVWh6rYXa0dyLVEvzYJSywqpzF0aqnXWZ4cyIosTwQlXIdcU+Y/8mjHnCgjs9OMzgX/AhTTNOnzPil8P9YqQSZ/9FGsfL8IQqqCUhMOP2BEKpnV/hrWwzVnRb5KROnrNy8pxbp2YvaigxSdF5Ue+8KH7uFolekegXCa8GtF10rVUfKRdSC2Y2fHa/3a71LdyuvWGTeIIQiyULiGuO5aqQS9PyjGtR7SXFGUvR911cSYMdNVuNoCRQJrYgSllHageywGl22oPwVHCaSJj4MQCrrsqNmS9JIOfZ/l8TnpE0Myjzas5jcsuZLIGgbJqcU32rXp8ZW+3Ivd+ONq1ZDXjvO03MQlrN3HMjVfowZ+W60ObB13Y11YnaOdBBxx31nJHXs4fO8NgbDXYE7Iz2AHywe5ZHdE8PciaFMcd+7ZrKbrIMezR0B4P+wPWOj4fOoD882O13ifGPMqO6/X6e0KA2rbZy1ha0WfQptj9TzingCtnnQq7dRt1xeU0x6O5G/IhBlj8H/2bK07VZucGgW/u5T7f4SdHZT1BLBwgeB3T7ugQAANkkAABQSwMEFAAICAgABQbvSgAAAAAAAAAAAAAAABcAAABnZW9nZWJyYV9kZWZhdWx0czNkLnhtbO1X3W7bIBi9Xp8Ccd/YxHbSVHGrqLvYpHba1JvdEvzFYXPABfLXV9s77JmGsUmdZonWKFWlqbmwD/Dxgc85fHaG16tZgRagNJcixaQTYgSCyYyLPMVzMzm/wNdXZ8McZA5jRdFEqhk1KU6qyM082+pE/X7Vh1aaXwr5hc5Al5TBPZvCjN5KRo0LnRpTXgbBcrns+KQdqfIgz01npTOM7IaETnEDLm26rUnLyIV3w5AE3+9u6/TnXGhDBQOM7GYzmNB5YbSFUMAMhEFmXUKK6YrryC5R0DEUKR5VzY8YNfEpjkgY4auzD0M9lUskxz+A2V6j5rCZ4xpBFWOHb2QhFVIpts+du+vYXWlRTqlFlg8XWtA1KLSgRTXqeujcSOZmu94JLTT4WLvOncygHombeMFnjkCkDZRWKIx0CZA5VD+cXbe0CznhWvm4vp/SEnbX4QLuzboAZKac/RSgLelRK1kFPvEsg8oX9Rx4EPUUXV1TXFJlZTaKM7t2jcGy8ftXFT4MGvJ3ZLDbFNDS4atrbwlh5T1KiMHAKdElA6eFu2/USE6lxj5OmZQq02hVS4/Wzf2xuS9blpjQ6og1u+iQA5qEhzTZTzKTgrMWyZ+F1UZbHisTsblawJbxk+P47iaJI5x0+zvmJ2H9I/EgJKRHuv/ZYeA5iIUlSiptq17YVM916FX3PSvi/UCanscGuDz2URRfoZGfN/Lho64HkQexB0nLSs9PJZ+VBWfcHPbHw5xm7uA2+n7z7SdTRCE5zhRhvKce9l/9CL6WkuF+JeshL9bag8fuiURGo54HfQ8uPBhsjHCg3spiPYVMSfFUcltdbcHrKnAMiS81CUkiZ5KE7LgkbgpHMuiFcS8+mWdOXTcOFN+qvjKqDGhORasK31QDz785eu/fHC9/vwkwG4K+VLht4+TdxqewsZ6rif2E/5uRm6Ft2uO3el08JX5TK5N/tXLQ+osS+L9BV38AUEsHCJGfMKHbAgAAjQ0AAFBLAwQUAAgICAAFBu9KAAAAAAAAAAAAAAAADAAAAGdlb2dlYnJhLnhtbO1b62/bRhL/nP4VC35K7kx7l08xkFK4zR1aIC2CuC2cHO6AFbmWeKFIlqRsScgffzP7oKiHZVmy3SBn1eo+uLuzM/ObB8dO//vZJCPXoqrTIh9Y7JRaRORxkaT5aGBNmyu7Z33/5rv+SBQjMaw4uSqqCW8Glo8r230wOnXDEOfSZGA5wyva84eRHftDz/Z8z7F7POI2dwMWCS9mV9HQImRWp6/z4lc+EXXJY3ERj8WEvyti3shDx01Tvj47u7m5OTXkT4tqdDYaDU9ndWIRuHpeDyzdeQ3HrWy6ceVyh1J2dvnLO3W8neZ1w/NYWATZmqZvvnvRv0nzpLghN2nSjEEIsMMiY5GOxsBogIMzXFUCt6WIm/Ra1LC3M5RMN5PSkst4js9fqB7JWn4skqTXaSKqgUVPPY/23M7Ht0hRpSJv9GKmiZ6Z4/rXqbhR52JPkvRoFIIW0jodZmJgXfGsBr7S/KoCmcKNqikM62aeiSGvzHh5IXYi/4Ml6ULgacCpEsXAgiud9KKTkNIT39cS6FK2SFMUmTyWki9fiEMdSk6wYapxoAkC9YiqOeqqxlGNpxpfrfHUdk8t9dQaT63x3B1c6vFONg2LbpdFBrzhFzR84tFNHnsdHhny8IUwvLxsXILXZvL62Hh6GKhhKBtG9WwP/xfhAEQS9GTnPizpiS5P7kE8sQ5VJad7EG1J+k60P0nnKEZbms42Nh3/FjaPk+6SUZctiQIt+SO/GyTde/G5gVFDkfl0X4rBihEe4wcOIB7SrR5AtUy3u0TyGJfymbN6K8KID4brExahO0KrdAjziQczPZgJiYtzPvOIS3AJc4n0OZ40YDRm34f9PiUMHRMwRcCzAYMO+jLfJ35A/BA3ol8IInkYhS+uhuvA18U514WvnHM9+KJ38+EgXx0Dl/DdQPbQnfhwvo/CJHLS7REvAkI44YeMuHAHGIeUwIkuHs8kE+BB8YcR5TxD4vQInAd848nUuYfp36qMwL3L8vtnJlD1tSZIPcbVmmYjJjXqxo1I6JLAaZUUoBi1pkKHhD4Jg46+TlBjgb9UGqqst6I0v9fRHKgtwMlQwgDoodyVFh3PKPJEq/LLhipB8t5S+HBBPIoRAkiRDtxoAW7htHpwfFSFAy4eIOCQAEPGLSqxSFnUaSvdscjKVg9SjGleTpsV0cWTxHSbAlbzTOZEen1SxJ9/WBO24HVj+rAIkollzqKSi5WU5kU/40ORQep3gUgg5Jpn6Fnk+VdF3hBjko4lj5PpU19M4yxNUp7/AYo3mcqv08lQVER2C2RRHoLbicmzZBQxaZYHwJJL4qKokot5DTghs0+igs1O6JzS7gekN9ePWHTq44zLIt8JAjixjjlC3PVW94CQ57c+kpTF9YVoGuC+JnwmaiO4UYWuRasGBz/XPxTZcqos0rz5kZfNtJJJMwSPCnk6z0eZkIKUGobsM/48LGYXKpoF6qzf5qWQzhts7bNIzoHuB7Q5lYrLaw1HPxZZUREwS8eHNHGk26Fq5Rq8b7uKyjVUrtBnIKn2OYscuUK2Q9XKVaB5dWHNPzPMU0MlrYkar8BMogbz22meNu/MoEnjz5p9vV5hohVsLTKEXpGfZ1lxI1qJrtJij02rf7YG4P5nUeUiw1tMa1G7b5UdyIM0RHOAybSY1utPYP173ozP8+SDGAG19xwdbAMXWV+ZiDidwBXUvFYAR8j8Doyp2USMKmHkoYxdqUfbJanLSvCkHgvRtEpS9tNdJnk0PPXruEpLRDkZKsy1QE7SmkN0WJHNimjct7cYqA1gDUMWRJHbg8AVQTxtLdSGN0Ma9sLAY67PeqHrMLCQRfvQo6wXBbibBpHHmN9asMNOvShw/Qj2hlHUgyczaVQACvmqudAj2wWLdjy3x7wooiz0Pf8pLLprv/vYx+x20LIHN5D54xBzthJbPCixMuON0GQ60/v4QQBrWSK2wRBbWp2DTPzShKriv+oapFnqcc0fIOjR4ms4Ta9NG+TMInzajItKvtjDjaFFg5mBTdZYFVHCIQOrBCcwhKvDq/8HcV1k05gXEJtnEMJn/3npvCJnkMv8nczbvk0WZEAwwZa3+nPKkyqNlT1mYiIgEK8+MXrYSknGXZABKYbI7fJ1R+3RI3ENAVmJBdZtSNpEE8KzcsyxgBHq1RmfY5zvZAry4F+KRKzMrqtcPezoN60vxrwUG/PLqNSMAVc5SFfCyqhMdn5Kk0TkrWYhqRD5NdArwPOSGdWVrTnFm6PnMJ0Z050502sWZmbmmEems3A6YXnCmyqdkXN5kgNSOmdtz2w4d03HMx3fdALTCU2nZzqRdIy+BqPS+Ibu8+lEdHUPYisqqWzoTY3szW13AGAZk7qKp7eonZmcyQNPywLm7IkCkwBmWAAjkxR0ZYOoJnyGuCN8WANcG3ERQ8TLlzVAZcI6YXSoUozx/3NwzbIOeZXORJsbg0mmC4jQbbyVHuBcxc8uu9uBRXcji+cQtqUbgABYKv9WCpGsQhJc2FyGjm6WsVTkmo8Aw814XnzSTgGNX2lTH4hPdQa/igH5wE2stWM2FL7irDsG77BNxUeRcq0sUq4V21b7T2XzMseoUdNUK1oZoLbRmw6yrzjWqg3c2Q7V7tTsLv2AD4cAINZ8Njv1W689IAts7++x5blH6cuh3lfpod2DPbR9Lxdttz7avsNJB2sf5bLlieixbXacy9aIvANI8Hpd1hig19Dk+J0UgAUwWOhBBOhi90fWktBxCcA3B69HSQBQSioBoMEyBWBrHwUvDbRHwJeOAJcGW4Cil5T8jSxemd781XHB5fI4NH1lwYWtBRf6FwQXLdiPWmfz4/Tz8ZvSjwn+7An1ExeTCc8TksvK+M85Fm+ADyzrWMu6LKfLpI0zHceVOKeNWZDqzfgWJqnoszdUCbvTuKPK7sa9k4Mt6qQrb8kPkcZ/rb6cGV9ubzrzg7KBO97f1MX+zBUvtap1pJMyS+O02SMNGKelqPSr+u8T/s8iG/Md+aWts4GDMoGttJ6Tgu1AsukOJN07Ldg75Xy8nGC1APWPDIu/cdF51dyoQR1Vd2oJPJedDs86932nsZeVJ3tfz3U3pDYLTzsh9ZN2L7ehyn4AVHVpPAPrDmDtclwHIOtpgdUNVm+nvJbRqjbA0sGwGyLttgRzZGjsUHsOjvf2XfYWiN2FsO3RcenJniY6ximIK6mKtdA4a8sv3V/NHISyDQrP+HoCF7a1/LL9dzRPgq/NOLkRI4+D18MFyf8DhD0IwHYkX08CqfcqQ9qGqAGZH46kzrnPQJoZ2MwNom57T6TdioOEEtsIhXQnkOhxuZb8rfFtOFotbP1W8bzGP7RYK2p1S0+ysvWHlMO/XtITAj8vbVmcfPXq3xvVLvXXFPctdbW79q5z9Z7rXF9pneuw0unyF1Vb66efxAG1009H/tbrGED1vnVAeRtVrOCvA9HHvUD08RAQfXwG0ROAKFz7fAWYutwLU5eHYOryGVOPhCnljp4MUmfdPwmV/xZC/xPVN/8DUEsHCFB8pC5MCgAAUzsAAFBLAQIUABQACAgIAAUG70pFzN5dGgAAABgAAAAWAAAAAAAAAAAAAAAAAAAAAABnZW9nZWJyYV9qYXZhc2NyaXB0LmpzUEsBAhQAFAAICAgABQbvSh4HdPu6BAAA2SQAABcAAAAAAAAAAAAAAAAAXgAAAGdlb2dlYnJhX2RlZmF1bHRzMmQueG1sUEsBAhQAFAAICAgABQbvSpGfMKHbAgAAjQ0AABcAAAAAAAAAAAAAAAAAXQUAAGdlb2dlYnJhX2RlZmF1bHRzM2QueG1sUEsBAhQAFAAICAgABQbvSlB8pC5MCgAAUzsAAAwAAAAAAAAAAAAAAAAAfQgAAGdlb2dlYnJhLnhtbFBLBQYAAAAABAAEAAgBAAADEwAAAAA="};
+var ggbApplet = new GGBApplet(param, '5.0');
+ggbApplet.setHTML5Codebase('https://app.geogebra.org/5.0/web3d/');
+window.onload = function() {
+	ggbApplet.inject('geogebra','preferHTML5');
+}
+
+ggb = document.ggb;
+quadrica = 'cone';
+plano = 'planoZ';
+ggbInit = false;
+
+function ggbOnInit(){
+	ggb = document.ggb;
+	ggb.enableRightClick(false);
+	ggb.enableLabelDrags(false);
+	document.getElementById('quadrica').value = 'cone';
+	document.getElementById('plano').value = 'planoZ';
+	document.getElementById('range').value = 0;
+	ggb.setValue('valor',0);
+	setEscala();
+	setFormula();
+	ggbInit = true;
+}
+
+function setValorPlano(){
+	var valor = document.getElementById('valorRange');
+	var range = document.getElementById('range');
+	valor.innerHTML = range.value;
+	if(ggbInit)
+		ggb.setValue('valor',range.value);
+}
+
+function setQuadrica(){
+	if(ggbInit){	
+		ggb.setVisible(quadrica,false);
+		quadrica = document.getElementById('quadrica').value;
+		ggb.setVisible(quadrica,true);
+		setIntersecao();
+		setEscala();
+	}
+	setFormula();
+}
+
+function setPlano(){
+	if(ggbInit){	
+		ggb.setVisible(plano,false);
+		plano = document.getElementById('plano').value;
+		ggb.setVisible(plano,true);
+		setIntersecao();
+		setEscala();
+	}
+}
+
+function setEscala(){
+	ggb.setCoordSystem(-6,6,-6,6,-6,6);
+	ggb.setCoordSystem(-6.5,6.5,-6.5,6.5);
+}
+
+function setFormula(){
+	var formula = document.querySelector('#formula');
+	var quadrica = document.getElementById('quadrica').value;
+	switch (quadrica){
+		case 'cone':
+		formula.innerHTML = '`rArr x^2/a^2+y^2/b^2-z^2/c^2=0`';
+		break;
+		case 'elipsoide':
+		formula.innerHTML = '`rArr x^2/a^2+y^2/b^2+z^2/c^2=1`';
+		break;
+		case 'paraboloideEliptico':
+		formula.innerHTML = '`rArr x^2/a^2+y^2/b^2-z=0`';
+		break;
+		case 'paraboloideHiperbolico':
+		formula.innerHTML = '`rArr x^2/a^2-y^2/b^2-z=0`';
+		break;
+		case 'paraboloideRevolucao':
+		formula.innerHTML = '`rArr x^2/a^2+y^2/a^2-z=0`';
+		break;
+		case 'hiperboloideUmaFolha':
+		formula.innerHTML = '`rArr x^2/a^2+y^2/b^2-z^2/c^2=1`';
+		break;
+		case 'hiperboloideDuasFolhas':
+		formula.innerHTML = '`rArr -x^2/a^2-y^2/b^2+z^2/c^2=1`';
+		break;
+		case 'cilindroEliptico':
+		formula.innerHTML = '`rArr x^2/a^2+y^2/b^2=1, -c<=z<=c`';
+		break;
+		case "cilindroHiperbolico":
+		formula.innerHTML = '`rArr x^2/a^2-y^2/b^2=1, -c<=z<=c`';
+		break;
+		case 'cilindroParabolico':
+		formula.innerHTML = '`rArr x^2/a^2+by=1, -c<=z<=c`';
+		break;
+	}
+	MathJax.Hub.Typeset(); //atualizar notacao asciimath
+}
+
+function setIntersecao(){
+	if(quadrica!='elipsoide')
+		setNaoElipsoide();
+	else
+		setElipsoide();
+}
+
+function setElipsoide(){
+	ggb.setVisible('interseccao',false);
+	if(plano=='planoZ'){
+		ggb.evalCommand('projecao = Translate[interZe, Vector[(0, 0, -valor)]]');
+		ggb.setVisible('interZe',true);
+		ggb.setVisible('interYe',false);
+		ggb.setVisible('interXe',false);
+		}
+	else if(plano=='planoY'){
+		ggb.evalCommand('projecao = Rotate[Translate[interYe,(0,-valor,0)],-90°,xAxis]');
+		ggb.setVisible('interYe',true);
+		ggb.setVisible('interZe',false);
+		ggb.setVisible('interXe',false);
+	}
+	else if(plano=='planoX'){
+		ggb.evalCommand('projecao = Rotate[Rotate[Translate[interXe,(-valor,0,0)],-90°,yAxis],-90°,zAxis]');
+		ggb.setVisible('interXe',true);
+		ggb.setVisible('interZe',false);
+		ggb.setVisible('interYe',false);
+	}
+}
+
+function setNaoElipsoide(){
+	ggb.setVisible('interseccao',true);
+	ggb.setVisible('interYe',false);
+	ggb.setVisible('interZe',false);
+	ggb.setVisible('interXe',false);
+	ggb.evalCommand('interseccao = Intersect['+plano+', '+quadrica+']');
+	if(plano=='planoZ'){
+		ggb.evalCommand('projecao = Translate[interseccao, Vector[(0, 0, -valor)]]');
+	}
+	else if(plano=='planoY'){
+		ggb.evalCommand('projecao = Rotate[Translate[interseccao,(0,-valor,0)],-90°,xAxis]');
+	}
+	else if(plano=='planoX'){
+		ggb.evalCommand('projecao = Rotate[Rotate[Translate[interseccao,(-valor,0,0)],-90°,yAxis],-90°,zAxis]');
+	}
+}
